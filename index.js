29c8ff25c31f80a02e825e952fa9f8ed228a1e55
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-01 14:57:06 +0530

    Adding a product model

--- index.js
+++ index.js
@@ -2,6 +2,8 @@ const express = require("express");
 const bodyParser = require("body-parser");
 const path = require("path");
 
+const errorController = require("./controllers/error");
+
 const app = express();
 const port = 5000;
 
@@ -17,9 +19,7 @@ app.use(express.static(path.join(__dirname, "public")));
 app.use("/admin", adminRoutes);
 app.use(shopRoute);
 
-app.use((req, res, next) => {
-  res.status(404).render("404", { pageTitle: "Page Not Found 🤔" });
-});
+app.use(errorController.get404);
 
 app.listen(port, () => {
   console.log(`server running on port: ${port}📡`);
