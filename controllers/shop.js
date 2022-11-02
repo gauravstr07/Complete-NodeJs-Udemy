0fce2e3344f41cbedf06394dadbfead295215b76
Gaurav Sutar <83762168+gauravstr07@users.noreply.github.com>
2022-11-02 15:01:49 +0530

    Passing Data with POST Request

--- controllers/shop.js
+++ controllers/shop.js
@@ -16,7 +16,7 @@ exports.getProduct = (req, res, next) => {
     res.render("shop/product-detail", {
       product: product,
       pageTitle: product.title,
-      path: '/products'
+      path: "/products",
     });
   });
 };
@@ -36,6 +36,12 @@ exports.getCart = (req, res, next) => {
     path: "/cart",
     pageTitle: "Your Cart🛒",
   });
+};
+
+exports.postCart = (req, res, next) => {
+  const prodId = req.body.productId;
+  console.log(prodId);
+  res.redirect("/cart");
 };
 
 exports.getOrders = (req, res, next) => {
